431acfac4e22873526ccf84acb680a929f2a92f6
Manuel Esteban Tapia <manuel.tapia.9318@hotmail.com>
2023-02-20 23:16:40 -0500

    quedamos por el capitulo 11 toda empezar de cero

--- src/es6/01-arrow-funtion.js
+++ src/es6/01-arrow-funtion.js
@@ -25,3 +25,51 @@ let lorem = "esto es un string \n" + "esto es otra linea";
 
 let lorem2 = `esta es una frase epica
 la continuacion de esa frase epica`;
+
+// String
+
+// se pueden colocar valores por defecto en los parametros para evitar el || or
+
+/*function newUser(name , age, country) {
+  var name = name || "Oscar";
+  var age = age || 32;
+  var country = country || "Co";
+}
+console.log(newUser());
+
+function newAdmin(name="manuel", age =30, country = "colombia") {
+  
+}*/
+
+// desectructurando
+//array
+
+let fruit = ["apple","banana"];
+let [a,b] = fruit;
+console.log(a,fruit[1]);
+
+// objeto
+
+let user = {username: "manuel",age:29};
+let {username,age} = user;
+
+// spread operator
+
+let person = {name:"Esteban",age:28 };
+let country = "colombia";
+
+let data = {id: 1,...person, country};
+
+//object literal
+
+function newuser(user,age,country,uid) {
+  return {
+    user,
+    age,
+    country,
+    id:uid
+  }
+}
+
+console.log(newuser("manuel",18,"col",1));
+
